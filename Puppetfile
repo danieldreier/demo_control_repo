e4263206599cfb5350f8cddd090f029866de325d
Daniel Dreier <d@puppetlabs.com>
2015-11-01 09:52:09 -0800

    initial commit

---
+++ Puppetfile
@@ -0,0 +1,13 @@
+forge "https://forgeapi.puppetlabs.com"
+mod "puppetlabs/apt"
+mod "puppetlabs/concat"
+mod "puppetlabs/firewall"
+mod "puppetlabs/ntp"
+mod "puppetlabs/stdlib"
+
+# zack/r10k deps
+mod "zack/r10k"
+mod "puppetlabs/gcc"
+mod "mhuffnagle/make"
+mod "puppetlabs/vcsrepo"
+mod "puppetlabs/git"
---
+++ site.pp
@@ -0,0 +1,18 @@
+Exec {
+  logoutput => 'on_failure',
+  path      => '/usr/bin:/usr/sbin:/bin:/sbin',
+}
+case $::osfamily {
+  'Debian': {
+    Package { subscribe => Exec['apt_update'] }
+  }
+}
+
+#hiera_include('classes')
+$default_packages = ['vim', 'mosh', 'tmux']
+
+node default {
+  package {$default_packages:
+    ensure => present
+  }
+}
